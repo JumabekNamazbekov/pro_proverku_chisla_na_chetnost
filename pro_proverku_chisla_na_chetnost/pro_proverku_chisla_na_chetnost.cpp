8db348b1227ac176333acf142031914995505cad
Jumabek Namazbekov <m.jumabek@list.ru>
2025-05-02 17:53:09 +0700

    Добавьте файлы проекта.

---
+++ pro_proverku_chisla_na_chetnost/pro_proverku_chisla_na_chetnost.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+
+	int san;
+	cout << "Введите число для проверки на чётность" << endl;
+	cin >> san;
+
+	if (san%2==0)
+	{
+		cout << "Число " << san << " чётное " << endl;
+	}
+	else
+	{
+		cout << "Число " << san << " не чётное " << endl;
+	}
+}
